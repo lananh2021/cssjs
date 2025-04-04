f739d7b28c1da30c79a3f4694294a7103ee2252b
Dự án Đông bình Dương <84308907+lananh2021@users.noreply.github.com>
2025-04-04 15:03:09 +0700

    Update redirectcopy1.js

--- redirectcopy1.js
+++ redirectcopy1.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://datbinhduong3s.vn/'>https://datbinhduong3s.vn</a>";
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://bdsla.vn/'>https://bdsla.vn/</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
